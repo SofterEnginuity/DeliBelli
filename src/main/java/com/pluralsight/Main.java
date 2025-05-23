e3ef28275060e2c843748de641eb0a40d0f71401
Marqae Morris <mamorris920@gmail.com>
2025-05-23 17:03:39 -0400

    test commit and push

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("This is the Deli-licious project!");
     }
 }
